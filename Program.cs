337f8dbfe629d2d3ad84f24b9526c937497a4bd1
Daniel Matos <daniel.matos@pucp.pe>
2017-04-30 13:28:23 -0500

    Add test2 on Program

--- Program.cs
+++ Program.cs
@@ -7,9 +7,8 @@ namespace TestAlgoritmo
         public static void Main()
         {
             Test t1 = new Test();
-            t1.LeerDatos("test1.txt");
+            t1.LeerDatos("test2.txt");
             Abeja.ConfigurarDatos(t1);
-
 
             AlgoritmoAbejas ba = new AlgoritmoAbejas(25, 250,5, 250, 1);
             ba.Asignacion();
